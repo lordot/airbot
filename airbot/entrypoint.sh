001fa29032e39362660736206fe6c8cfce7a8f5a
lordot <w.dmkvo@gmail.com>
2023-09-06 15:27:57 +0300

    fixed createsuperuser

--- airbot/entrypoint.sh
+++ airbot/entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-python manage.py migrate && python manage.py collectstatic --noinput && python manage.py createsuperuser --noinput
+python manage.py migrate && python manage.py collectstatic --noinput
+python manage.py createsuperuser --noinput
 
 exec uvicorn airbot.asgi:application --host 0.0.0.0
