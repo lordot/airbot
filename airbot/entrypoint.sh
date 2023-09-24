3d397f7211591a1a3de239bf5c5761f720e942c1
lordot <w.dmkvo@gmail.com>
2023-09-24 16:10:06 +0300

    init commit 8

--- airbot/entrypoint.sh
+++ airbot/entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-./manage.py collectstatic --noinput
+python manage.py collectstatic --noinput
 
-./manage.py migrate
-./manage.py createsuperuser --no-input
+python manage.py migrate
+python manage.py createsuperuser --no-input
 
 exec uvicorn airbot.asgi:application --host 0.0.0.0
