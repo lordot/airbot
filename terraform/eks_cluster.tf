6996961c3e8fa901467947559cbdab38b9a4d921
lordot <w.dmkvo@gmail.com>
2023-09-28 23:04:34 +0300

    fixed public endpoint access

--- terraform/eks_cluster.tf
+++ terraform/eks_cluster.tf
@@ -1,15 +1,15 @@
+#data "aws_eks_cluster" "cluster" {
+#  name = module.eks.cluster_id
+#}
+#
+#data "aws_eks_cluster_auth" "cluster" {
+#  name = module.eks.cluster_id
+#}
+#
 #provider "kubernetes" {
-#  host = data.aws_eks_cluster.my-eks-cluster.endpoint
-#  token = data.aws_eks_cluster_auth.my-eks-cluster-auth.token
-#  cluster_ca_certificate = base64decode(data.aws_eks_cluster.my-eks-cluster.certificate_authority[0].data)
-#}
-#
-#data "aws_eks_cluster" "my-eks-cluster" {
-#  name = module.eks.cluster_name
-#}
-#
-#data "aws_eks_cluster_auth" "my-eks-cluster-auth" {
-#  name = module.eks.cluster_name
+#  host                   = data.aws_eks_cluster.cluster.endpoint
+#  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+#  token                  = data.aws_eks_cluster_auth.cluster.token
 #}
 
 module "eks" {
@@ -18,9 +18,18 @@ module "eks" {
 
   cluster_name = var.cluster_name
   cluster_version = "1.28"
+  cluster_endpoint_public_access = true
 
   vpc_id = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
+
+  aws_auth_users = [
+    {
+      userarn  = "arn:aws:iam::340482041692:user/admin"
+      username = "admin"
+      groups   = ["system:masters"]
+    }
+  ]
 
   eks_managed_node_groups = {
 #    blue = {
